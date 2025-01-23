a52cf7acff3f7f1586b06363d9cfa0f45a7fcfd6
AnWandGerannt <8649767-AnWandGerannt@users.noreply.replit.com>
2025-01-23 16:16:04 +0000

    User checkpoint: Deploy to Cloud Run: Add necessary dependencies, configure ports, and set up deployment.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { exec } = require("child_process");
 const express = require("express");
 const server = express();
+//const keep_alive = require("./keep_alive.js");
 
 exec("python test.py");
