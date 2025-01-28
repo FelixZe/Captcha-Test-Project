41f68c7059b6736abbc801d0237e1b8c78680fdc
Steve Lang <stevelang@macbookair.speedport.ip>
2025-01-28 02:21:12 +0100

    x

--- test.py
+++ test.py
@@ -186,4 +186,4 @@ def feedback():
 # Run server
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host="0.0.0.0", port=5100, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
